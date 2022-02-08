ea2870d75980858b55a2ce9ab7b27a5db6438101
JJ240 <tundersro@gmail.com>
2022-02-08 16:23:41 +0700

    first commit

---
+++ src/main/java/nvc/bcit/icefactory/IceFactoryApplication.java
@@ -0,0 +1,14 @@
+package nvc.bcit.icefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class IceFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceFactoryApplication.class, args);
+	}
+
+}
